39af1352d4386d85533ff14f95ffdb4777317d00
Azralii <111491491+Azralii@users.noreply.github.com>
2023-09-24 17:35:32 +0200

    Jag korrigerade coden

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,6 @@ const Navbar = ()=>{
 return <div>
     <ul style={{display:"flex",width:'100%',height:'70px',alignItems:'center',backgroundColor:'black',justifyContent:'space-evenly'}}>
         <li><Link to={'/'}>Cinema</Link></li>
-        <li><Link to={'/Booking'}>Booking</Link></li>
     </ul>
 </div>
 }
